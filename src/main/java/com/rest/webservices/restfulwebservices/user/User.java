f9c07bc79e684c6919c598e13e65ebb4073f9ba4
baniyapratik <baniyapratik@gmail.com>
2023-03-25 23:55:00 -0700

    connecting rest api to h2

--- src/main/java/com/rest/webservices/restfulwebservices/user/User.java
+++ src/main/java/com/rest/webservices/restfulwebservices/user/User.java
@@ -1,12 +1,18 @@
 package com.rest.webservices.restfulwebservices.user;
 
 import com.fasterxml.jackson.annotation.JsonProperty;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.Id;
 import jakarta.validation.constraints.Past;
 import jakarta.validation.constraints.Size;
 
 import java.time.LocalDate;
 
+@Entity(name="user_details")
 public class User {
+    @Id
+    @GeneratedValue
     private Integer id;
     @Size(min=2, message = "Name should have at least 2 characters.")
     @JsonProperty("user_name")
@@ -20,6 +26,8 @@ public class User {
         this.name = name;
         this.birthDate = birthDate;
     }
+
+    public User() {}
 
     public Integer getId() {
         return id;
